f868e2a952b34410c89f852cd24d32a49c79cee0
vladNiderkwel <niderkwelll@gmail.com>
2022-07-06 11:15:42 +0400

    changes 10

--- DbDemo/src/main/resources/db/changelog/create-insert/changelog-insert.sql
+++ DbDemo/src/main/resources/db/changelog/create-insert/changelog-insert.sql
@@ -9,17 +9,18 @@ insert into  user ( name, email, hashcode )
 insert into  games ( title, description, price, imagename )
     values ( 'The long dark',
     'The Long Dark - это исследовательская игра-симулятор выживания',
-    600, '')
+    600, '1_The Long Dark.jpg')
 );
 
 --changeset liquibase:c
 insert into  games ( title, description, price, imagename )
     values ( 'Terraria',
     'Terraria - это пиксельная "песочница"',
-    350, '')
+    350, '25_Terraria.jpg')
 );
 
 --changeset liquibase:d
 insert into  roles ( idofuser, role )
     values ( 1, 'admin')
 );
+
