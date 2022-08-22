ed3fa4888e6411f3f9e932e8edad72dd16b2dea1
ilkalinin <ilkalinin@neoflex.ru>
2022-08-22 16:01:34 +0300

    12

--- DbDemo/src/main/resources/db/changelog/create-insert/changelog-insert.sql
+++ DbDemo/src/main/resources/db/changelog/create-insert/changelog-insert.sql
@@ -1,26 +1,22 @@
 --liquibase formatted sql
 
 --changeset liquibase:a
-insert into  user ( name, email, hashcode )
-    values ( 'admin', 'admin@admin.admin', '123')
-);
+insert into  users ( name, email, hashcode,iconname )
+    values ( 'admin', 'admin@admin.admin', '123','');
 
 --changeset liquibase:b
 insert into  games ( title, description, price, imagename )
     values ( 'The long dark',
     'The Long Dark - это исследовательская игра-симулятор выживания',
-    600, '1_The Long Dark.jpg')
-);
+    600, '1_The Long Dark.jpg');
 
 --changeset liquibase:c
 insert into  games ( title, description, price, imagename )
     values ( 'Terraria',
     'Terraria - это пиксельная "песочница"',
-    350, '25_Terraria.jpg')
-);
+    350, '25_Terraria.jpg');
 
 --changeset liquibase:d
 insert into  roles ( idofuser, role )
-    values ( 1, 'admin')
-);
+    values ( 1, 'admin');
 
